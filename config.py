294d66b38652037170874092c0b2a5919d3dded8
vidrasenpai <88034866+vidrasenpai@users.noreply.github.com>
2022-12-25 01:48:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5138955231:AAFgYi80H9KvlHk1y85F8bFd9vY6n36gFI0")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5773373505:AAFxQuJ6klB7bVd-30n1XVvQi87DaTKVLXk")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "7375040"))
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "7375040"))
 API_HASH = os.environ.get("API_HASH", "4166e18db5a7880136d41ceb0aa20971")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001653338217"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001836566126"))
 
 # OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1880970848"))
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1880970848"))
 OWNER = os.environ.get("OWNER", "Vidraplay")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://fjavswki:j5zb7KdXYvfcX9_DjKSGF3FqpTaeVfCu@satao.db.elephantsql.com/fjavswki")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://vvjolkjp:zBgJLBKi7m0FPyXtVt6djFV1YeCOUooD@satao.db.elephantsql.com/vvjolkjp")
 
 # Username CH & Group
 CHANNEL = os.environ.get("CHANNEL", "ometvhxh")
