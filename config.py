e51ce41bb2585b3bf0c684c04f0f91363eb54966
vidrasenpai <88034866+vidrasenpai@users.noreply.github.com>
2023-10-10 21:16:24 +0700

    config.py

--- config.py
+++ config.py
@@ -31,12 +31,12 @@ OWNER = os.environ.get("OWNER", "Vidraplay")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://veypbhdx:lNIrKGr-dczwzFZWnByiOu49Xz85wk3j@satao.db.elephantsql.com/veypbhdx")
 
 # Username CH & Group
-CHANNEL = os.environ.get("CHANNEL", "ometvhxh")
-GROUP = os.environ.get("GROUP", "hv015")
+CHANNEL = os.environ.get("CHANNEL", "ometvi")
+GROUP = os.environ.get("GROUP", "huntersxyz")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001787258151"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001814927193"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001571621547"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 
